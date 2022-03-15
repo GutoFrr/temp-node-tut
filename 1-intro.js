f549fe885d6d5529abcd84264954c36d50ecc8f9
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-03-15 19:59:00 -0300

    first commmit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const tamanhoPipi = 9
+
+if (tamanhoPipi < 10) {
+    console.log("pouco pipi");
+} else if (tamanhoPipi > 10) {
+    console.log("muito pipi");
+}
+
+console.log("hora do node garai!")
